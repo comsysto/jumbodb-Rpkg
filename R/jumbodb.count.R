0523d1c931c1ba05f29da9a32db180847701443c
Dr. rer. nat. Markus Schmidberger <markus.schmidberger@googlemail.com>
2013-10-21 10:09:26 +0200

    initiale code structure for count added

--- R/jumbodb.count.R
+++ R/jumbodb.count.R
@@ -1,4 +1,25 @@
 jumbodb.count <-
 function(host, collection, query='{"limit": 1}', user=NULL, password=NULL, authenticate="basic"){
+  
+  require(httr)
+  require(rjson)
+  
+  #check host string
+  if( !grepl("http", host) ){
+    stop("http is missing in host")
+  }
+  
+  url <- paste(host, '/jumbodb/jumbodb/rest/query/collections', sep="")
+  conf <- c(add_headers(Connection = "keep-alive"), accept_json())
+  
+  if( !is.null(user) && !is.null(password) ){
+    conf <- append(conf, authenticate(user, password, authenticate))
+  }
+  
+  out <- "Comming soon"
+  # ToDo
+  
   print("Comming soon")
+  
+  return(out)
 }
